c8bed11f205becf43950739b1d5c0aff3c8b8fbc
myeongin seo <westco2@naver.com>
2024-02-07 17:35:54 +0900

    3nd

---
+++ UI_C_2/bid.js
@@ -0,0 +1,10 @@
+document.getElementById("bbt2").addEventListener('click',(e)=>{
+    document.querySelector(".inbuy").style.height="80px";
+    //document.querySelector(".tybo").classList.toggle('tybo');
+    document.getElementById("bbt2").classList.toggle('tybo');
+    console.log(e.target.classList);
+})
+//즉시구매 페이지로 이동
+// document.getElementById("bbt1").addEventListener('click',()=>{
+    
+// })
---
+++ UI_C_3/settle.js
@@ -0,0 +1,14 @@
+
+var vovo = document.querySelector('.new-address');
+
+document.querySelector('.nonc').addEventListener('click',()=>{
+     
+     vovo.style.height = '500px';
+})
+
+document.querySelector('.npnc').addEventListener('click',()=>{
+     
+     vovo.style.height = '0px';
+     
+})
+
